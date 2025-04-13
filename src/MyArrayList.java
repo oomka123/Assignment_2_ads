d96d45c380c0b74553bd17d0f6bbba112b805f81
Kazbek <k.ahmetov123@gmail.com>
2025-04-13 19:20:10 +0600

    MyArrayList created

---
+++ src/MyArrayList.java
@@ -0,0 +1,167 @@
+public class MyArrayList<T extends Comparable<T>> implements MyList<T> {
+    private Object[] data;
+    private int size;
+
+    public MyArrayList() {
+        data = new Object[5];
+        size = 0;
+    }
+
+    private void increaseCapacity() {
+        if (size == data.length) {
+            Object[] newData = new Object[data.length * 2];
+            for (int i = 0; i < data.length; i++) {
+                newData[i] = data[i];
+            }
+            data = newData;
+        }
+    }
+
+    @Override
+    public void add(T item) {
+        increaseCapacity();
+        data[size++] = item;
+    }
+
+    @Override
+    public void add(int index, T item) {
+        if (index < 0 || index > size) throw new IndexOutOfBoundsException();
+        increaseCapacity();
+        for (int i = size; i > index; i--) {
+            data[i] = data[i - 1];
+        }
+        data[index] = item;
+        size++;
+    }
+
+    @Override
+    public void set(int index, T item) {
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
+        data[index] = item;
+    }
+
+    @Override
+    public void addFirst(T item) {
+        add(0, item);
+    }
+
+    @Override
+    public void addLast(T item) {
+        add(item);
+    }
+
+    @Override
+    public T get(int index) {
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
+        return (T) data[index];
+    }
+
+    @Override
+    public T getFirst() {
+        if (size == 0) throw new IllegalStateException("List is empty");
+        return get(0);
+    }
+
+    @Override
+    public T getLast() {
+        if (size == 0) throw new IllegalStateException("List is empty");
+        return get(size - 1);
+    }
+
+    @Override
+    public void remove(int index) {
+        if (index < 0 || index >= size) throw new IndexOutOfBoundsException();
+        for (int i = index; i < size - 1; i++) {
+            data[i] = data[i + 1];
+        }
+        data[--size] = null;
+    }
+
+    @Override
+    public void removeFirst() {
+        remove(0);
+    }
+
+    @Override
+    public void removeLast() {
+        if (size == 0) throw new IllegalStateException("List is empty");
+        remove(size - 1);
+    }
+
+    @Override
+    public void sort() {
+        for (int i = 0; i < size - 1; i++) {
+            for (int j = 0; j < size - i - 1; j++) {
+                T a = (T) data[j];
+                T b = (T) data[j + 1];
+                if (a.compareTo(b) > 0) {
+                    Object temp = data[j];
+                    data[j] = data[j + 1];
+                    data[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    @Override
+    public int indexOf(Object object) {
+        for (int i = 0; i < size; i++) {
+            if (object == null && data[i] == null) return i;
+            if (object != null && object.equals(data[i])) return i;
+        }
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Object object) {
+        for (int i = size - 1; i >= 0; i--) {
+            if (object == null && data[i] == null) return i;
+            if (object != null && object.equals(data[i])) return i;
+        }
+        return -1;
+    }
+
+    @Override
+    public boolean exists(Object object) {
+        return indexOf(object) != -1;
+    }
+
+    @Override
+    public Object[] toArray() {
+        Object[] array = new Object[size];
+        for (int i = 0; i < size; i++) {
+            array[i] = data[i];
+        }
+        return array;
+    }
+
+    @Override
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            data[i] = null;
+        }
+        size = 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public java.util.Iterator<T> iterator() {
+        return new java.util.Iterator<T>() {
+            private int position = 0;
+
+            @Override
+            public boolean hasNext() {
+                return position < size;
+            }
+
+            @Override
+            public T next() {
+                return (T) data[position++];
+            }
+        };
+    }
+}
